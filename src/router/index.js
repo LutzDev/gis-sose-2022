700c496df7a8bea70ca7f077334c18d693bc4adc
PhilippOesch <philipp.oeschger@hs-furtwangen.de>
2022-03-14 21:16:46 +0100

    router update

--- src/router/index.js
+++ src/router/index.js
@@ -2,14 +2,16 @@ import {createRouter, createWebHistory} from "vue-router";
 import Home from "../views/Home.vue";
 import Exe1 from "../views/Exe1.vue";
 
+const basePath= "/gis-sose-2022"
+
 const routes = [
     {
-        path: '/Home',
+        path: `${basePath}/Home`,
         name: 'Home',
         component: Home
     },
     {
-        path: '/Exe1',
+        path: `${basePath}/Exe1`,
         name: 'Exe1',
         component: Exe1
     }
