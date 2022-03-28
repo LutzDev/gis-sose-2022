1725521b3b926b951dabd55c478c7a4777f6ac82
Philipp Oeschger <kontakt@philipp-oeschger.de>
2022-03-28 04:34:23 +0200

    Task2 Setup

--- src/views/Task2.vue
+++ src/views/Task2.vue
@@ -1,42 +1,201 @@
 <template>
-  <Header v-bind:example-button="false" v-bind:script-button="true"/>
-  <h1>HTML Grundlagen</h1>
-  <Text>In dieser Aufgabe erstellen Sie eine stillose HTML-Seite (<b>ohne</b> CSS und Javascript).</Text>
+  <Header
+    v-bind:example-button="false"
+    v-bind:script-button="true"
+    v-bind:print-button="true"
+  />
+  <h1>CSS</h1>
+  <Text
+    >In dieser Aufgabe erstellen stilisieren sie die in der letzten Aufgabe
+    erstellten HTML-Seiten mit CSS.</Text
+  >
   <Sections>
-    <Section headline="Projektstruktur">
+    <Section headline="Allgemein">
       <List>
-        <Checkbox>Legen Sie zu Beginn für Ihr gewähltes Thema einen Projektordner mit <b>passendem Namen</b> an. (Bsp. <highlight>airbnb-clone_mustermann</highlight>)</Checkbox>
-        <Checkbox>Überlegen Sie sich und realisieren Sie eine Seitenstruktur bestehend aus <b>mindestens zwei</b> HTML-Seiten, welche zu Ihrem Thema passen</Checkbox>
+        <Checkbox
+          >Binden Sie das Styling
+          <a target="_blank" href="https://www.w3schools.com/css/css_howto.asp"
+            >extern</a
+          >
+          ein. Verwenden Sie <b>kein inline-Style</b> und
+          <b>kein HTML-Style-Tag</b>.</Checkbox
+        >
+        <Checkbox
+          >Verwenden Sie <b>eine</b> übergreifende CSS-Datei für alle Ihre
+          HTML-Seiten</Checkbox
+        >
       </List>
     </Section>
-    <Section headline="Seiteninhalt">
+    <Section headline="Grundlagen">
       <List>
-        <Checkbox>Schreiben Sie im <highlight>&lt;head&gt;...&lt;/head&gt;</highlight> neben einem geeigneten <highlight>&lt;title&gt;</highlight> <b>vier</b> <highlight>&lt;meta&gt; Tags</highlight></Checkbox>
-        <Checkbox>Verwenden Sie im <highlight>&lt;body&gt;...&lt;/body&gt;</highlight> die HTML-Tags <highlight>&lt;header&gt;, &lt;main&gt;, &lt;footer&gt;</highlight></Checkbox>
-        <Checkbox>Benutzen Sie mindestens <b>sechs weitere</b> unterschiedliche HTML-Tags (Bsp.: <highlight>&lt;h1&gt;, &lt;p&gt;, &lt;img&gt;, &lt;table&gt;, ... </highlight>)</Checkbox>
+        <Checkbox
+          >Wenden Sie das
+          <a
+            target="_blank"
+            href="https://www.w3schools.com/css/css_boxmodel.asp"
+            >Box-Model</a
+          >
+          an (margin, padding, etc.).</Checkbox
+        >
+        <Checkbox
+          >Verwenden Sie <b>3 der 4</b> in der Vorlesung vorgestellten
+          <a href="https://heintz-s.github.io/GIS/lecture/css/4">Selektoren</a>
+          mindestens einmal.</Checkbox
+        >
+        <Checkbox
+          ><a
+            target="_blank"
+            href="https://heintz-s.github.io/GIS/lecture/css/5"
+            >Kombinieren</a
+          >
+          Sie mindestens <b>3-mal</b> in Ihrer CSS-Datei Selektoren.</Checkbox
+        >
+        <Checkbox
+          >Betten Sie mindestens eine Webfont in ihrer Webseite ein. Das können
+          Sie fremd gehostet z. B. über das Einbinden einer Google Font machen
+          (wie in der Vorlesung gezeigt) oder falls Sie bereits wissen, wie das
+          geht selbst gehostet mit @font-face.
+        </Checkbox>
+        <Checkbox
+          >Betten Sie mindestens eine selbst oder fremdgehostete
+          <a target="_blank" href="https://heintz-s.github.io/GIS/lecture/css/7"
+            >Webfont</a
+          >
+          in ihrer Webseite ein.
+        </Checkbox>
+        <Checkbox
+          >Experimentieren Sie mit verschiedenen
+          <a target="_blank" href="https://heintz-s.github.io/GIS/lecture/css/7"
+            >Maßeinheiten und/ oder Farbdefinitionen</a
+          >.</Checkbox
+        >
+        <Checkbox
+          >Arbeiten Sie mit der CSS-Eigenschaft
+          <a target="_blank" href="https://heintz-s.github.io/GIS/lecture/css/9"
+            >position</a
+          >.</Checkbox
+        >
       </List>
+    </Section>
+    <Section headline="Vertiefung">
+      <List>
+        <Checkbox
+          >Wenden Sie
+          <a
+            target="_blank"
+            href="https://developer.mozilla.org/de/docs/Web/CSS/Pseudo-classes"
+            >Pseudoklassen</a
+          >
+          an.</Checkbox
+        >
+        <Checkbox
+          >Erstellen Sie <b>zusätzlich</b> Hover-Effekte mit der Pseudoklasse
+          <a
+            target="_blank"
+            href="https://www.w3schools.com/cssref/sel_hover.asp"
+            >:hover</a
+          >.</Checkbox
+        >
+        <Checkbox
+          >Arbeiten Sie mit
+          <a
+            target="_blank"
+            href="https://developer.mozilla.org/de/docs/Web/CSS/Pseudo-elements"
+            >Pseudoelementen</a
+          >.</Checkbox
+        >
+        <Checkbox
+          >Nutzen Sie
+          <a
+            target="_blank"
+            href="https://heintz-s.github.io/GIS/lecture/css2/4"
+            >CSS-Funktionen</a
+          >.</Checkbox
+        >
+        <Checkbox
+          >Verwenden Sie
+          <a
+            target="_blank"
+            href="https://heintz-s.github.io/GIS/lecture/css2/5"
+            >Animationen und/ oder Transitionen</a
+          >
+          auf Ihrer Webseite.</Checkbox
+        >
+        <Checkbox
+          >Gestallten Sie Ihre Webseiten
+          <a
+            target="_blank"
+            href="https://heintz-s.github.io/GIS/lecture/css2/6"
+            >Adaptiv/Responsive</a
+          >. Hierbei können Sie falls nötig von
+          <a href="https://www.w3schools.com/css/css_rwd_mediaqueries.asp"
+            >Media queries</a
+          >
+          Gebrauch machen.</Checkbox
+        >
+        <Checkbox
+          >Achten Sie darauf, dass Sie ihre Webseiten möglichst
+          <a
+            target="_blank"
+            href="https://heintz-s.github.io/GIS/lecture/css2/7"
+            >Barrierefrei</a
+          >
+          gestallten.</Checkbox
+        >
+        <Checkbox
+          >Verwenden Sie möglichst nur CSS-Eigenschaften verwenden, die in den
+          gängigen Browsern (Chrome, Firefox, Safari, Edge) unterstützt werden
+          (siehe
+          <a target="_blank" href="https://caniuse.com/">Can I Use</a
+          >).</Checkbox
+        >
+      </List>
+    </Section>
+    <Section headline="Hinweise und Tipps">
+      <Text
+        >Sie müssen nicht auf der HTML-Struktur der letzten Aufgabe beharren.
+        Machen Sie wenn nötig Stukturenänderunge, wenn es dem Stil der Seite
+        unterstützt. Styling und Struktur der Seite sollten eng
+        zusammenarbeiten.</Text
+      >
     </Section>
   </Sections>
   <Footer>
     <FooterItem>
-      <IconHeadline headline="Nützliche Links" v-bind:info-icon="true"/>
+      <IconHeadline headline="Nützliche Links" v-bind:info-icon="true" />
       <List>
-        <li>Philipp Oeschger: <a href="mailto:p.oeschger@hs-furtwangen.de">p.oeschger@hs-furtwangen.de</a></li>
-        <li>Lutz Weigold: <a href="l.weigold@hs-furtwangen.de">l.weigold@hs-furtwangen.de</a></li>
+        <li>
+          Philipp Oeschger:
+          <a href="mailto:p.oeschger@hs-furtwangen.de"
+            >p.oeschger@hs-furtwangen.de</a
+          >
+        </li>
+        <li>
+          Lutz Weigold:
+          <a href="l.weigold@hs-furtwangen.de">l.weigold@hs-furtwangen.de</a>
+        </li>
       </List>
     </FooterItem>
     <FooterItem>
-      <IconHeadline headline="Kontaktaufnahme" v-bind:contact-icon="true"/>
+      <IconHeadline headline="Kontaktaufnahme" v-bind:contact-icon="true" />
       <List>
-        <li>Philipp Oeschger: <a href="mailto:p.oeschger@hs-furtwangen.de">p.oeschger@hs-furtwangen.de</a></li>
-        <li>Lutz Weigold: <a href="l.weigold@hs-furtwangen.de">l.weigold@hs-furtwangen.de</a></li>
+        <li>
+          Philipp Oeschger:
+          <a href="mailto:p.oeschger@hs-furtwangen.de"
+            >p.oeschger@hs-furtwangen.de</a
+          >
+        </li>
+        <li>
+          Lutz Weigold:
+          <a href="l.weigold@hs-furtwangen.de">l.weigold@hs-furtwangen.de</a>
+        </li>
       </List>
     </FooterItem>
     <FooterItem>
-      <IconHeadline headline="Abgabetermin" v-bind:deadline-icon="true"/>
+      <IconHeadline headline="Abgabetermin" v-bind:deadline-icon="true" />
       <List>
-        <li>MIB: Sonntags 18:00 Uhr</li>
-        <li>OMB: Mittwochs 18:00 Uhr</li>
+        <li>MIB: 10.04.22 - 18:00 Uhr</li>
+        <li>OMB: 18.04.22 18:00 Uhr</li>
       </List>
     </FooterItem>
   </Footer>
@@ -55,24 +214,36 @@ import Checkbox from "../components/Checkbox.vue";
 
 export default {
   name: "Task2",
-  components: {Checkbox, Highlight, List, Section, Sections, Text, Footer, Header, IconHeadline},
+  components: {
+    Checkbox,
+    Highlight,
+    List,
+    Section,
+    Sections,
+    Text,
+    Footer,
+    Header,
+    IconHeadline,
+  },
   mounted() {
-    let darkMode = document.createElement('script')
-    darkMode.setAttribute('src', 'scripts/darkMode.js')
-    document.head.appendChild(darkMode)
+    let darkMode = document.createElement("script");
+    darkMode.setAttribute("src", "scripts/darkMode.js");
+    document.head.appendChild(darkMode);
 
-    let partyjs = document.createElement('script')
-    partyjs.setAttribute('src', 'https://cdn.jsdelivr.net/npm/party-js@latest/bundle/party.min.js')
-    document.head.appendChild(partyjs)
+    let partyjs = document.createElement("script");
+    partyjs.setAttribute(
+      "src",
+      "https://cdn.jsdelivr.net/npm/party-js@latest/bundle/party.min.js"
+    );
+    document.head.appendChild(partyjs);
 
-    let checkBox = document.createElement('script')
-    checkBox.setAttribute('src', 'scripts/checkBox.js')
-    checkBox.setAttribute('type', 'module')
-    document.head.appendChild(checkBox)
-  }
-}
+    let checkBox = document.createElement("script");
+    checkBox.setAttribute("src", "scripts/checkBox.js");
+    checkBox.setAttribute("type", "module");
+    document.head.appendChild(checkBox);
+  },
+};
 </script>
 
 <style scoped>
-
 </style>
